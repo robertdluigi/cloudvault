0bdb9f6623424cbc8400e37cfaae53b756917cd0
Robert <robertdluigi@gmail.com>
2024-12-08 18:52:16 +0200

    first commit

---
+++ backend/main.go
@@ -0,0 +1,17 @@
+// create a basic go that says hello
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
